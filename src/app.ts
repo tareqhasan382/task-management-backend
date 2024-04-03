39651d40378923bda81e4038ce9a90c091a1626e
tareqhasan382 <tareqhasan382@gmail.com>
2024-04-03 15:50:40 +0600

    cors added

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,12 @@ import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import httpStatus from "http-status";
 import { UserRoute } from "./app/modules/users/user.route";
 import { TaskRoute } from "./app/modules/tasks/task.route";
-
-app.use(cors());
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
